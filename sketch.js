b10a13d4469d6cfbc59c7d93dadfd7e3f3ef4e71
Joaogui2009 <104601738+Joaogui2009@users.noreply.github.com>
2022-04-28 20:18:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var box;
+function setup() {
+box = createSprite(200,200,30,30)
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  drawSprites();
+  if (keysDown(RIGHT_ARROW)) { box.position.x
+    = box.position. x +5; }
+
+
+
+
+
+}
+
+
+
+
+
+
+
